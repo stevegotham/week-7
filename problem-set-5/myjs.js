dcc97f03ae1cb7272d44218415f883c6b6725b58
stevegotham <g23design@gmail.com>
2016-05-20 12:14:44 -0600

    finished

--- problem-set-5/myjs.js
+++ problem-set-5/myjs.js
@@ -1,9 +1,13 @@
+// Write a function alphabetSoup that takes a single string parameter and returns the string with the letters in alphabetical order (ie. hello becomes ehllo). Assume numbers and punctuation symbols will not be included in the string.
+
 function alphabetSoup (str) {
   var arr = str.split('');
   arr.sort()
   return arr.join('')
 
 }
+
+// Write a function vowelCount that takes a single string parameter and returns the number of vowels the string contains (ie. "All cows eat grass" would return 5).
 
 function vowelCount (str) {
   var count = 0;
